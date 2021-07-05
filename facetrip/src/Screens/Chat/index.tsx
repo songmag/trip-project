5364b8d36d77a5ae6c578e067286996c5c9da860
SeungNyungLee <maple0110@naver.com>
2021-07-05 21:57:55 +0900

    [2021-07-05] Chat Screen
    Screen basic code set for navigator

---
+++ facetrip/src/Screens/Chat/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Styled from 'styled-components/native';
+
+const Container = Styled.View`
+  flex: 1;
+`;
+
+const Chat = () => {
+  return (
+    <Container></Container>
+  );
+};
+
+export default Chat;
