51b633ee251f586471886b899e918da3acc28936
SeungNyungLee <maple0110@naver.com>
2021-07-22 18:49:50 +0900

    [2021-07-22] rebase rn

--- facetrip/src/Screens/Home/index.tsx
+++ facetrip/src/Screens/Home/index.tsx
@@ -3,15 +3,21 @@ import Styled from 'styled-components/native';
 import {StackNavigationProp} from '@react-navigation/stack';
 
 import HeaderComponent from '~/Components/HeaderComponent';
+<<<<<<< HEAD
 import FilterComponent from '~/Components/FilterComponent';
 import GuideListComponent from '~/Components/GuideListComponent';
 
+=======
+import ModalButtonComponent from '~/Components/ModalButtonComponent';
+import FilterBoardComponent from '~/Components/FilterBoardComponent';
+>>>>>>> a44c5b60 ([2021-07-16] FaceTrip)
 
 const Container = Styled.View`
   flex: 1;
   background-color: white;
 `;
 
+<<<<<<< HEAD
 type NavigationProp = StackNavigationProp<ShowGuideNaviParam, 'ShowGuide'>
 
 <<<<<<< HEAD
@@ -46,6 +52,51 @@ const Home = ({navigation}: Props) => {
         setSelectAreaPropFunction={setSelectAreaPropFunction}
         setSelectLanguagePropFunction={setSelectLanguagePropFunction}/>
       <GuideListComponent navigation={navigation}/>
+=======
+const FilterContainer = Styled.View`
+  width: 100%;
+  height: 150px;
+  align-items: center;
+`;
+
+const ModalContainer = Styled.View`
+  flex: 1;
+  width: 90%;
+  flex-direction: row;
+`;
+
+const ListContainer = Styled.View`
+  flex: 1;
+`;
+
+const Home = () => {
+  const [selectArea, setSelectArea] = useState<Array<String>>([]);
+  // 현재 선택된 Area
+  const [selectLanguage, setSelectLanguage] = useState<string | undefined>(undefined);
+  // 현재 선택된 Language
+
+  return (
+    <Container>
+      <HeaderComponent imagePath={require('~/Assets/Images/logoReplacement.png')} headerTitle={"로고 위치"}/>
+      <FilterContainer>
+        <ModalContainer>
+          <ModalButtonComponent
+            modalTitle={"지역 검색"}
+            style={{
+              flex: 1,
+              alignItems: "flex-start"
+            }}/>
+          <ModalButtonComponent
+            modalTitle={"사용 언어"}
+            style={{
+              flex: 1,
+              alignItems: "flex-end"
+            }}/>
+        </ModalContainer>
+        <FilterBoardComponent/>
+      </FilterContainer>
+      <ListContainer/>
+>>>>>>> a44c5b60 ([2021-07-16] FaceTrip)
     </Container>
   );
 };
